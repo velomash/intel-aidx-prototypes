63e642ea18ca85869eed0d4d7063bb8f608319e8
Adam Trimble <adam.trimble@intel.com>
2016-08-22 10:38:51 -0700

    better css based off top-nav height

--- app/scripts/global-nav.js
+++ app/scripts/global-nav.js
@@ -1,1 +1,15 @@
-console.log('yo');
+///////////////////////////////////////////////////////////////
+//                                                           //
+//                 Intel Global Navigation                   //
+//                    by: Adam Trimble                       //
+//                 © Intel Corporation 2016                  //
+//                                                           //
+///////////////////////////////////////////////////////////////
+
+class IntelGlobalNavigation {
+    constructor(navDomElement) {
+    }
+}
+
+const navElement = document.getElementsByClassName('global-navigation')[0];
+export default new IntelGlobalNavigation(navElement);
