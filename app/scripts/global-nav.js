a9e3a0554f74037c8ab300e3bac6cdf053b2223d
Adam Trimble <adam.trimble@intel.com>
2016-08-23 10:51:57 -0700

    flyout system taking shape

--- app/scripts/global-nav.js
+++ app/scripts/global-nav.js
@@ -6,20 +6,36 @@
 //                                                           //
 ///////////////////////////////////////////////////////////////
 
+import delegate from 'delegate';
+
 class IntelGlobalNavigation {
     constructor(navDomElement) {
         this.nav = navDomElement;
-
-        // init nav events
-        $(this.nav).on('click touchstart', '[data-flyout]', this.openFlyout.bind(this));
-        $(this.nav).on('click touchstart', '.shader', this.closeFlyout.bind(this));
+        delegate(this.nav, '[data-flyout]', 'click', this.onFlyoutTrigger.bind(this));
+        delegate(this.nav, '[data-flyout]', 'touchend', this.onFlyoutTrigger.bind(this));
+        delegate(this.nav, '.shader', 'click', this.closeFlyout.bind(this));
+        delegate(this.nav, '.shader', 'touchend', this.closeFlyout.bind(this));
     }
-    openFlyout(event) {
-        const panelId = event.currentTarget.getAttribute('data-flyout');
-        this.nav.classList.add('flyout-active');
+    onFlyoutTrigger(event) {
+        const targetFlyout = document.getElementById(event
+            .delegateTarget
+            .getAttribute('data-flyout'));
+        if (targetFlyout === this.activeFlyout) {
+            this.closeFlyout();
+        } else {
+            this.openFlyout(targetFlyout);
+        }
     }
-    closeFlyout(event) {
-        this.nav.classList.remove('flyout-active');
+    openFlyout(targetFlyout) {
+        if (this.activeFlyout) {
+            this.nav.classList.remove(`${this.activeFlyout.id}-active`);
+        }
+        this.activeFlyout = targetFlyout;
+        this.nav.classList.add('flyout-active', `${this.activeFlyout.id}-active`);
+    }
+    closeFlyout() {
+        this.nav.classList.remove('flyout-active', `${this.activeFlyout.id}-active`);
+        this.activeFlyout = undefined;
     }
 }
 
