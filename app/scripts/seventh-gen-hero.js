b30323d40e3750759b1cde63f02ee655a7a3b7da
Adam Trimble <adam.trimble@intel.com>
2016-09-13 10:15:22 -0700

    better tweening structure

--- app/scripts/seventh-gen-hero.js
+++ app/scripts/seventh-gen-hero.js
@@ -16,6 +16,7 @@ class chipParticleSystem {
         this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
         this.container.appendChild(this.renderer.domElement);
         this.particles = this.makeParticles();
+        this.particles.forEach(particle => this.initParticleTween(particle));
         this.animate();
     }
 
@@ -34,44 +35,39 @@ class chipParticleSystem {
         const material = new THREE.SpriteMaterial({
             map: new THREE.CanvasTexture(canvas),
             blending: THREE.AdditiveBlending,
+            opacity: 0,
         });
         const particles = [];
         for (var i = 0; i < 1000; i++) {
             const particle = new THREE.Sprite(material);
-            this.initParticle(particle);
+            const vector = new THREE.Vector3();
+            vector.setX(randomNumberInRange(-2000, 2000));
+            vector.setY(randomNumberInRange(-1000, 1000));
+            vector.setZ(randomNumberInRange(-2000, 2000));
+            particle.start = vector.clone().setLength(randomNumberInRange(500, 1000));
+            particle.end = vector.clone().setLength(randomNumberInRange(1500, 2000));
+            particle.position.fromArray(particle.start.toArray());
+            particle.scale.x = particle.scale.y = Math.random() * 12 + 5;
             this.scene.add(particle);
             particles.push(particle);
         }
-        return particles;
-    }
-
-    getRandomPosition() {}
-
-    initParticle(particle) {
-        const vector = new THREE.Vector3();
-        vector.setX( Math.random() * 4000 - 2000 );
-        vector.setY( Math.random() * 1000 - 500 );
-        vector.setZ( Math.random() * 4000 - 2000 );
-        particle.start = vector.clone().setLength(randomNumberInRange(500, 1000));
-        particle.end = vector.clone().setLength(randomNumberInRange(1500, 2000));
-        particle.position.fromArray( particle.start.toArray() );
-        particle.scale.x = particle.scale.y = Math.random() * 12 + 5;
-
-        new TWEEN.Tween(particle.position)
-            .to(particle.end, 10000)
-            .start();
-
-        // new TWEEN.Tween(particle.scale)
-            // .delay(delay)
-            // .to({
-                // x: 0.01,
-                // y: 0.01
-            // }, 50000)
-            // .start();
 
         function randomNumberInRange(startRange, endRange) {
             return Math.random() * (endRange - startRange - 1) + startRange;
         }
+        return particles;
+    }
+
+
+    initParticleTween(particle) {
+        const tween = new TWEEN.Tween(particle.position)
+            .to(particle.end, 10000)
+            .start();
+        new TWEEN.Tween(particle.material)
+            .to({
+                opacity: 1,
+            }, 500)
+            .start();
     }
 
     // the animate funciton runs 60fps and renders
