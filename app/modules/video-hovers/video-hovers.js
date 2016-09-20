adbbcd24d519f32a1de30211d27e78a05b472d73
Adam Trimble <adam.trimble@intel.com>
2016-09-20 11:09:36 -0700

    better video hover positioning

--- app/modules/video-hovers/video-hovers.js
+++ app/modules/video-hovers/video-hovers.js
@@ -6,6 +6,7 @@ class videoHover {
             this.initVideo();
             element.addEventListener('mouseenter', this.startVideo.bind(this));
             element.addEventListener('mouseleave', this.stopVideo.bind(this));
+            window.addEventListener('resize', this.initVideo.bind(this));
         }
     }
     initVideo() {
@@ -16,12 +17,13 @@ class videoHover {
         const ratio = videoWidth / videoHeight;
         const newVideoWidth = height * ratio * 1.5;
         const newVideoHeight = newVideoWidth / ratio;
-        const offset = videoWidth - width / 2;
+        const offsetLeft = (videoWidth - width) / 2;
+        const offsetTop = (videoHeight - height) / 2;
         this.video.volume = 0;
         this.video.style.width = `${newVideoWidth}px`;
         this.video.style.height = `${newVideoHeight}px`;
-        this.video.style.left = `-${offset}px`;
-        this.video.style.top = '-100px';
+        this.video.style.left = `-${offsetLeft}px`;
+        this.video.style.top = `-${offsetTop}px`;
         this.video.style.opacity = 0;
     }
     startVideo() {
