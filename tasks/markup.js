2ed03ae30e483eb6e4cde3b09848afe32a231561
Adam Trimble <adam.trimble@intel.com>
2016-09-29 14:25:44 -0700

    watch the pages better

--- tasks/markup.js
+++ tasks/markup.js
@@ -58,7 +58,7 @@ module.exports = (gulp, options, plugins) => {
         const templateWatcher = gulp.watch([
             options.getPath('src', 'partials', '**/*.hbs'),
             options.getPath('src', 'data', '**/*.json'),
-            options.getPath('src', 'pages', '**/*.json'),
+            options.getPath('src', 'pages', '**/*.hbs'),
             options.getPath('src', 'helpers', '**/*.js'),
             options.getPath('src', 'layouts', '**/*.hbs'),
         ], ['markup:dev']);
