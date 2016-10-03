7f4ca8834df9e2f111546d887b6fc018c172887e
Adam Trimble <adam.trimble@intel.com>
2016-10-03 11:24:43 -0700

    update copy for blades

--- app/modules/idf-2016/idf-2016.js
+++ app/modules/idf-2016/idf-2016.js
@@ -1,16 +1,20 @@
-const idfBlade = document.getElementById('idf-2016-blade');
-if (idfBlade) {
-    let isInView = false;
-    const onLoad = window.addEventListener('load', checkIfScrolledIntoView);
-    const onScroll = window.addEventListener('scroll', checkIfScrolledIntoView);
-}
-
-function checkIfScrolledIntoView() {
-    const bladeTop = idfBlade.getBoundingClientRect().top;
-    const bladeBottom = idfBlade.getBoundingClientRect().bottom;
-    if (bladeTop > 50 && bladeBottom < window.innerHeight - 50) {
-        window.removeEventListener('load', onLoad, false);
-        window.removeEventListener('scroll', onScroll, false);
-        idfBlade.classList.add('visible');
+class idfBlade {
+    constructor() {
+        this.idfBlade = document.getElementById('idf-2016-blade');
+        if (this.idfBlade) {
+            this.onLoad = window.addEventListener('load', this.checkIfScrolledIntoView.bind(this));
+            this.onScroll = window.addEventListener('scroll', this.checkIfScrolledIntoView.bind(this));
+        }
+    }
+    checkIfScrolledIntoView() {
+        const bladeTop = this.idfBlade.getBoundingClientRect().top;
+        const bladeBottom = this.idfBlade.getBoundingClientRect().bottom;
+        if (bladeTop > 50 && bladeBottom < window.innerHeight - 50) {
+            window.removeEventListener('load', this.onLoad, false);
+            window.removeEventListener('scroll', this.onScroll, false);
+            this.idfBlade.classList.add('visible');
+        }
     }
 }
+
+export default new idfBlade();
