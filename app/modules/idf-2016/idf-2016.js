eb5817796d39635da64701c5e52b8cfea0c9f5b6
Adam Trimble <adam.trimble@intel.com>
2016-10-06 12:11:11 -0700

    better idf triggering / misc

--- app/modules/idf-2016/idf-2016.js
+++ app/modules/idf-2016/idf-2016.js
@@ -3,17 +3,17 @@ import throttle from 'lodash.throttle';
 class idfBlade {
     constructor() {
         this.idfBlade = document.getElementById('idf-2016-blade');
+        this.onScroll = throttle(this.checkIfScrolledIntoView.bind(this), 75);
         if (this.idfBlade) {
-            const checkScroll = throttle(this.checkIfScrolledIntoView.bind(this), 75);
-            this.onLoad = window.addEventListener('load', this.checkIfScrolledIntoView.bind(this));
-            this.onScroll = window.addEventListener('scroll', checkScroll);
+            window.addEventListener('load', this.onScroll);
+            window.addEventListener('scroll', this.onScroll);
         }
     }
     checkIfScrolledIntoView() {
         const bladeTop = this.idfBlade.getBoundingClientRect().top;
         const bladeBottom = this.idfBlade.getBoundingClientRect().bottom;
-        if (bladeTop > -50 && bladeBottom < window.innerHeight + 50) {
-            window.removeEventListener('load', this.onLoad, false);
+        if (bladeTop < window.innerHeight / 3) {
+            window.removeEventListener('load', this.onScroll, false);
             window.removeEventListener('scroll', this.onScroll, false);
             this.idfBlade.classList.add('visible');
         }
