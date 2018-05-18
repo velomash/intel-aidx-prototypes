ce6d6e1e44bef1a7dbf0907da94edf7167666835
Adam Trimble <adam.trimble@gmail.com>
2018-05-18 01:09:27 -0700

    promotional one up

--- app/modules/drone-hub/scroll-events.js
+++ app/modules/drone-hub/scroll-events.js
@@ -19,6 +19,64 @@ class ScrollBehaviors {
       }
     });
   }
+
+  setupTextFades() {
+    const sections = document.querySelectorAll('section');
+    for (var i=0, l=sections.length; i<l; i++) {
+      new ScrollMagic.Scene({
+        triggerElement: sections[i],
+        // triggerHook: 'onLeave',
+      })
+      .setClassToggle(sections[i], 'activate-text')
+      .addTo(this.controller);
+    }
+  }
+
+  heroAnimation() {
+    const hero = document.querySelector('header.hero');
+    if (!hero) { return false; }
+    const animationDuration = 1250;
+    const text = {
+      futureOf: hero.querySelector('h5'),
+      commercial: hero.querySelector('.commercial'),
+      drones: hero.querySelector('.drones'),
+    };
+    const clouds = [...hero.querySelectorAll('.cloud')];
+    const drone = hero.querySelector('.drone');
+    const timelineAnimation = new TimelineLite();
+    timelineAnimation
+      .to(clouds[0], animationDuration / 3, {
+        x: '-=200',
+        y: '-=50',
+        opacity: 0,
+      }, 0)
+      .to(clouds[1], animationDuration / 3, {
+        x: '+=200',
+        y: '-=50',
+        opacity: 0,
+      }, 0)
+      .to(text.futureOf, animationDuration / 3, {
+        y: '-=100',
+        opacity: 0,
+      }, 2)
+      .to(text.commercial, animationDuration / 3, {
+        y: '-=75',
+        opacity: 0,
+      }, 2)
+      .to(text.drones, animationDuration / 3, {
+        y: '+=75',
+        opacity: 0,
+      }, 2);
+    const scene = new ScrollMagic
+      .Scene({
+        triggerElement: hero,
+        triggerHook: 'onLeave',
+        duration: animationDuration,
+      })
+      .setTween(timelineAnimation)
+      .setPin(hero)
+      .addTo(this.controller);
+  }
 }
 
 export default new ScrollBehaviors();
